ec2910c2cba184804a497b865c243b94f1644fea
Kathleen <kathleenbryan24@gmail.com>
2018-07-15 09:45:18 -0400

    remove alert

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,43 +81,32 @@ var light = new _three.AmbientLight(0x444444);
 scene.add(light);
 
 function initButtons() {
-
     var btnRed = document.getElementById("red");
     btnRed.addEventListener('click', function () {
         cubeColor(red);
     });
-
     var btnYellow = document.getElementById("yellow");
     btnYellow.addEventListener('click', function () {
         cubeColor(yellow);
     });
-
     var btnGreen = document.getElementById("green");
     btnGreen.addEventListener('click', function () {
         cubeColor(green);
     });
-
     var btnBlue = document.getElementById("blue");
     btnBlue.addEventListener('click', function () {
         cubeColor(blue);
     });
-
     var btnBlue = document.getElementById("purple");
     btnBlue.addEventListener('click', function () {
         cubeColor(purple);
     });
-
     var btnRandom = document.getElementById("random");
     btnRandom.addEventListener('click', function () {
         cubeColor(randomColor);
     });
 }
-
 initButtons();
-
-function cubeColor(color) {
-    alert('color');
-}
 
 var currentColor = 0x7a7a7a;
 var red = 0xFF0000;
@@ -126,8 +115,8 @@ var green = 0x00ff00;
 var blue = 0x0000bb;
 var purple = 0xf000ff;
 
+// create option for random color selection
 var colorArray = [red, yellow, green, blue, purple];
-
 var randomColor;
 
 var size = 0.5;
@@ -150,12 +139,16 @@ setInterval(function () {
     var mesh = new _three.Mesh(geometry, material);
     mesh.position.y = Math.random() * 10 - 5; //spawn new cube at random x, y and z
     mesh.position.x = Math.random() * 10 - 5;
-    // mesh.position.z = Math.random()* 10 - 5;
+    // mesh.position.z = Math.random()* 10 - 5;  //commented out to demonstrate all cubes appear at same initial size
     mesh.rotation.y = Math.random() * 10 - 5;
     mesh.rotation.x = Math.random() * 10 - 5;
     mesh.rotation.z = Math.random() * 10 - 5;
     scene.add(mesh);
 }, 1000);
+
+// The following functions, grab all instances of Mesh in leiu of looping through all mesh material objects,
+// and cube objects, looping through an array and assining new values to each, every time there is user input 
+
 
 function cubeSize(passedSize) {
     scene.traverse(function (node) {
--- src/index.js
+++ src/index.js
@@ -8,41 +8,21 @@ const light = new AmbientLight(0x444444);
 
 scene.add(light);
 
-
-
-
-
-
-
 function initButtons() { 
-
     var btnRed = document.getElementById("red");
     btnRed.addEventListener( 'click',function(){cubeColor(red)});
-
     var btnYellow = document.getElementById("yellow");
     btnYellow.addEventListener( 'click',function(){cubeColor(yellow)});
-
     var btnGreen = document.getElementById("green");
     btnGreen.addEventListener( 'click',function(){cubeColor(green)});
-
     var btnBlue = document.getElementById("blue");
     btnBlue.addEventListener( 'click',function(){cubeColor(blue)});
-
     var btnBlue = document.getElementById("purple");
     btnBlue.addEventListener( 'click',function(){cubeColor(purple)});
-
     var btnRandom = document.getElementById("random");
     btnRandom.addEventListener( 'click',function(){cubeColor(randomColor)});
 }
-
 initButtons(); 
-
-
-function cubeColor(color) {
-    alert('color'); 
-} 
-
-
 
 var currentColor = 0x7a7a7a;
 var red = 0xFF0000;
@@ -51,12 +31,9 @@ var green = 0x00ff00;
 var blue = 0x0000bb; 
 var purple = 0xf000ff; 
 
-
+// create option for random color selection
 var colorArray = [red, yellow, green, blue, purple]; 
-
 var randomColor; 
-
-
 
 var size = 0.5;
 var currentSize; 
@@ -75,17 +52,20 @@ const geometry = new BoxBufferGeometry( size, size, size );
 const material = new MeshBasicMaterial( { color: currentColor } );
 
 
-
 setInterval(function() {
   const mesh = new Mesh( geometry, material );
   mesh.position.y = Math.random()* 10 - 5; //spawn new cube at random x, y and z
   mesh.position.x = Math.random()* 10 - 5; 
-  // mesh.position.z = Math.random()* 10 - 5;
+  // mesh.position.z = Math.random()* 10 - 5;  //commented out to demonstrate all cubes appear at same initial size
   mesh.rotation.y = Math.random()* 10 - 5;
   mesh.rotation.x = Math.random()* 10 - 5; 
   mesh.rotation.z = Math.random()* 10 - 5;
   scene.add(mesh)
 }, 1000);
+
+
+// The following functions, grab all instances of Mesh in leiu of looping through all mesh material objects,
+// and cube objects, looping through an array and assining new values to each, every time there is user input 
 
 
 function cubeSize(passedSize) {
@@ -106,13 +86,6 @@ scene.traverse( function( node ) {
     	}
 	});
 } 
-
-
-
-
-
-
-
 
 
 camera.position.z = 5; 
